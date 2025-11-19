3fb37c150fe5089ab59709a3ad0ade59e4be6336
exca mutiara <121956511+xxcamnxx@users.noreply.github.com>
2025-11-19 13:09:20 +0700

    week7-diffie-hellman

---
+++ praktikum/week7-diffie-hellman/src/diffie_hellman.py
@@ -0,0 +1,20 @@
+import random
+
+# parameter umum (disepakati publik)
+p = 23  # bilangan prima
+g = 5   # generator
+
+# private key masing-masing pihak
+a = random.randint(1, p-1)  # secret Alice
+b = random.randint(1, p-1)  # secret Bob
+
+# public key
+A = pow(g, a, p)
+B = pow(g, b, p)
+
+# exchange public key
+shared_secret_A = pow(B, a, p)
+shared_secret_B = pow(A, b, p)
+
+print("Kunci bersama Alice :", shared_secret_A)
+print("Kunci bersama Bob   :", shared_secret_B)
