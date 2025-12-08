6b548114b1fdafe7cd70ba782d991f639a07cb04
exca mutiara <121956511+xxcamnxx@users.noreply.github.com>
2025-12-08 21:08:54 +0700

    Implement signature verification in Verifikasi.py

---
+++ praktikum/week9-digital-signature/src/Verifikasi.py
@@ -0,0 +1,5 @@
+try:
+    pkcs1_15.new(public_key).verify(h, signature)
+    print("Verifikasi berhasil: tanda tangan valid.")
+except (ValueError, TypeError):
+    print("Verifikasi gagal: tanda tangan tidak valid.")
