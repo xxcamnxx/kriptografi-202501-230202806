98d315b761b9de6c2fec4abc832b1bbbd1991061
exca mutiara <121956511+xxcamnxx@users.noreply.github.com>
2025-12-08 21:08:09 +0700

    Update and rename code-example.py to Generate Key.py

---
+++ praktikum/week9-digital-signature/src/Generate Key.py
@@ -0,0 +1,16 @@
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
+
+# Generate pasangan kunci RSA
+key = RSA.generate(2048)
+private_key = key
+public_key = key.publickey()
+
+# Pesan yang akan ditandatangani
+message = b"Hello, ini pesan penting."
+h = SHA256.new(message)
+
+# Buat tanda tangan dengan private key
+signature = pkcs1_15.new(private_key).sign(h)
+print("Signature:", signature.hex())
