0f537abc7f4fe5c3556895f82ae1455450b2c91d
exca mutiara <121956511+xxcamnxx@users.noreply.github.com>
2025-12-08 21:09:28 +0700

    week9-digital-signature

---
+++ praktikum/week9-digital-signature/src/Uji Modifikasi.py
@@ -0,0 +1,9 @@
+# Modifikasi pesan
+fake_message = b"Hello, ini pesan palsu."
+h_fake = SHA256.new(fake_message)
+
+try:
+    pkcs1_15.new(public_key).verify(h_fake, signature)
+    print("Verifikasi berhasil (seharusnya gagal).")
+except (ValueError, TypeError):
+    print("Verifikasi gagal: tanda tangan tidak cocok dengan pesan.")
